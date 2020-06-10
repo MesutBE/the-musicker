700fa86343daf0f61d1ca35439930a916ccfc2a2
mesutBE <gelukkig.zijn.be@gmail.com>
2020-06-11 00:41:38 +0200

    A user can click on one artist to see all the albums by that artist, with the songs for each album listed

--- client/interactions/interactions.js
+++ client/interactions/interactions.js
@@ -1,10 +1,18 @@
 document.querySelector('#root').addEventListener('click', async (e) => {
     // debugger
-    if (e.target.classList.contains('link')) {
+    if (e.target.classList.contains('table-link')) {
         const text = e.target.innerText;
         
         const data = await doFetch.get(`/api/${text}`);
         
         UI.addChild(text, data);
+    } else if (e.target.classList.contains('table-child-link')) {
+        const text = e.target.parentNode.childNodes[0].innerText;
+        const id = e.target.childNodes[0].innerText;
+
+        const data = await doFetch.get(`/api/${text}/${id}`);
+
+        console.log(data);
+        
     }
 });
--- client/view/view.js
+++ client/view/view.js
@@ -8,9 +8,20 @@ class UI {
         const ul = document.createElement('ul');
 
         data.forEach(element => {
-            root.innerHTML += `
-            <ul id="table-${element.name}"><a href="#" class="link">${element.name}</ul>
-            `
+            if (!element.name.includes('sqlite_')) {
+                if (element.name === 'artists' ||
+                    element.name === 'albums' ||
+                    element.name === 'tracks' ||
+                    element.name === 'playlists') {
+                root.innerHTML += `
+                    <ul id="table-${element.name}"><a href="#" class="table-link">${element.name}</ul>
+                    `
+                } else {
+                    root.innerHTML += `
+                    <ul id="table-${element.name}">${element.name}</ul>
+                    `
+                }
+            }
         });
 
         root.appendChild(ul);
@@ -18,11 +29,30 @@ class UI {
 
     static addChild(text, data) {
         const tableName = document.getElementById(`table-${text}`)
-        
-        data.forEach(element => {
-            tableName.innerHTML += `
-            <li>${element.Name}</li>
+        if (text == 'artists') {
+            data.forEach(element => {
+                tableName.innerHTML += `
+            <li class="table-child-link"><span style="display: none">${element.ArtistId}</span>${element.Name}</li>
             `
-        })
+            })
+        } else if (text == 'albums') {
+            data.forEach(element => {
+                tableName.innerHTML += `
+            <li class="table-child-linkx"><span style="display: none">${element.AlbumId}</span>${element.Title}</li>
+            `
+            })
+        } else if (text == 'tracks') {
+            data.forEach(element => {
+                tableName.innerHTML += `
+            <li class="table-child-linkx"><span style="display: none">${element.TrackId}</span>${element.Name}</li>
+            `
+            })
+        } else if (text == 'playlists') {
+            data.forEach(element => {
+                tableName.innerHTML += `
+            <li class="table-child-linkx"><span style="display: none">${element.PlaylistId}</span>${element.Name}</li>
+            `
+            })
+        }
     }
 }
